ec0126b6a49dcfe283342ad13d7af01fc584ab12
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-05-07 12:58:01 +0200

    another commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
 app.get("/", (req, res) => {
-  res.send("Home Page");
+  res.send("Home Page ya Djibuji");
 });
-app.get("/about", (req, res) => {
-  res.send("Nzakayangana");
-});
+
 app.listen(5000, () => {
-  console.log("server is listening on port 5000...");
+  console.log("server is listening on the port 5000...");
 });
