037ab9cd9dc1e668be7a2bdd8970db2902f7ab1c
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-05-07 14:51:18 +0200

    another commit

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Home Page ya Djibuji");
 });
-
 app.listen(5000, () => {
   console.log("server is listening on the port 5000...");
 });
+// app.get("/api/products", (req, res) => {
+//   res.send("Data Received");
+// });
+mongoose
+  .connect(
+    "mongodb+srv://Alexandre:ndanyuzwe2@atlascluster.7z2yy0l.mongodb.net/"
+  )
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(() => console.log("not connected!"));
