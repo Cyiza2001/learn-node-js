ed2c2cbf3ef718c7943a44750d16930d6cd5cc2f
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-05-07 17:20:03 +0200

    another commit

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const production = require("./models/product.model.js");
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Home Page ya Djibuji");
 });
-app.listen(5000, () => {
-  console.log("server is listening on the port 5000...");
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await product.create(req.body);
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
 });
-// app.get("/api/products", (req, res) => {
-//   res.send("Data Received");
-// });
 mongoose
   .connect(
     "mongodb+srv://Alexandre:ndanyuzwe2@atlascluster.7z2yy0l.mongodb.net/"
   )
   .then(() => {
     console.log("Connected to the database!");
+    app.listen(5000, () => {
+      console.log("server is listening on the port 5000...");
+    });
   })
   .catch(() => console.log("not connected!"));
