01bbc18f896fbea6bde73b676e5559b9cfcaf1c3
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-05-20 12:40:56 +0200

    another commit

--- app.js
+++ app.js
@@ -1,80 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const product = require("./models/product.model.js");
-// const productController = require("./controllers/product.controller.js");
 const productRoutes = require("./routes/product.route.js");
 const authRoutes = require("./routes/auth.route.js");
+const path = require("path");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage: storage });
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
-
-// const crypto = require("crypto");
-// const secretKey = crypto.randomBytes(64).toString("hex");
-// console.log(secretKey);
-
-// app.get("/", (req, res) => {
-//   res.send("Home Page ya Djibuji");
-// });
-// app.get("/api/products", async (req, res) => {
-//   try {
-//     const products = await product.find();
-//     res.status(200).json(products);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ message: err.message });
-//   }
-// });
-
-// app.get("/api/products/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const Product = await product.findById(id);
-
-//     res.status(200).json(Product);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ message: err.message });
-//   }
-// });
-
-// app.post("/api/products", async (req, res) => {
-//   try {
-//     console.log(req.body);
-//     const Product = await product.create(req.body);
-//     res.status(200).json(Product);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ message: err.message });
-//   }
-// });
-//update products in the database
-// app.put("/api/products/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const Product = await product.findByIdAndUpdate(id, req.body);
-//     if (!Product) return res.status(404).json({ message: "product not found" });
-//     const updatedProduct = await product.findById(id);
-//     res.status(200).json(updatedProduct);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ message: err.message });
-//   }
-// });
-//delete a product from the database
-// app.delete("/api/products/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const Product = await product.findByIdAndDelete(id, req.body);
-//     if (!Product) return res.status(404).json({ message: "product not found" });
-//     res.status(200).json({ message: "product deleted successfuly" });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ message: err.message });
-//   }
-// });
+app.set("view engine", "ejs");
+app.get("/upload", (req, res) => {
+  res.render("upload");
+});
+app.post("/upload", upload.single("image"), (req, res) => {
+  res.send("image uploaded");
+});
 mongoose
   .connect(
     "mongodb+srv://Alexandre:ndanyuzwe2@atlascluster.7z2yy0l.mongodb.net/"
